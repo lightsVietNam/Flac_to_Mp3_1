f85ee3d7d6dd68820b546284e5ec2d645466e1b8
lightsVietNam <xuanquang170593@gmail.com>
2017-06-06 10:24:53 +0700

    up
    up

---
+++ Flac_To_Mp3_Callback-master/index.js
@@ -0,0 +1,6 @@
+// const Convert = require('./convert_callback').Converter;
+//
+// let myConvert = new Convert();
+// let srcFolder = __dirname + '/flac';
+// let desFolder = __dirname + '/mp3';
+// myConvert.runner(srcFolder,desFolder,{'limits' : 9});
